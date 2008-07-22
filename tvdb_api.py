0f200b60faea59902ada2cc1a829de97beb508cb
dbr <dbr.onix@gmail.com>
2008-07-22 13:57:36 +0100

    Bumped version to 0.2 for inital PyPi release

--- tvdb_api.py
+++ tvdb_api.py
@@ -18,7 +18,7 @@ Example usage:
 'Cabin Fever'
 """
 __author__ = "dbr/Ben"
-__version__ = "0.1"
+__version__ = "0.2"
 
 class _Ddict(dict):
     """Lazy-dict, automatically creates multidimensional dicts
