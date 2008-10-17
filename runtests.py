293fd3eda151993841a1ea28d73c9ff98a6cbcaf
dbr <dbr.onix@gmail.com>
2008-10-17 22:00:31 +1030

    Abilty to output test results as XML

--- runtests.py
+++ runtests.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
+import sys
 import unittest
 
 import tvdb_api
 import tvnamer
 
-suite = unittest.TestLoader().loadTestsFromTestCase(tvnamer.test_name_parser)
-unittest.TextTestRunner(verbosity=2).run(suite)
+def main():
+    output_xml = True; import xmlrunner
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--xml":
+            import xmlrunner
+            output_xml = True
+    
+    
+    suite = unittest.TestSuite([
+        unittest.TestLoader().loadTestsFromTestCase(tvnamer.test_name_parser),
+        unittest.TestLoader().loadTestsFromTestCase(tvdb_api.test_tvdb)
+    ])
+    
+    runner = unittest.TextTestRunner()
+    if output_xml:
+        runner = xmlrunner.XmlTestRunner(sys.stdout)
+    else:
+        runner = unittest.TextTestRunner(verbosity=2)
+    
+    runner.run(suite)
 
-suite = unittest.TestLoader().loadTestsFromTestCase(tvdb_api.test_tvdb)
-unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == '__main__':
+    main()
