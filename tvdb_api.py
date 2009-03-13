ed991d357c57b421cfa24f45cdc404ff5f3a5d5b
dbr <dbr.onix@gmail.com>
2009-03-13 22:15:37 +1030

    Revert removal of _can_int, as the item isn't necessarily a string (int(2).isalpha() will not work..) This reverts commit 6e3329fac7a4ebb1691791b652ea0fa0280179f3.

--- tvdb_api.py
+++ tvdb_api.py
@@ -35,6 +35,21 @@ from tvdb_ui import BaseUI, ConsoleUI
 from tvdb_exceptions import (tvdb_error, tvdb_userabort, tvdb_shownotfound,
     tvdb_seasonnotfound, tvdb_episodenotfound, tvdb_attributenotfound)
 
+def _can_int(x):
+    """Takes a string, checks if it is numeric.
+    >>> _can_int("2")
+    True
+    >>> _can_int("A test")
+    False
+    """
+    try:
+        int(x)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 class ShowContainer(dict):
     """Simple dict that holds a series of Show instances
     """
@@ -61,7 +76,7 @@ class Show(dict):
             return dict.__getitem__(self.data, key)
 
         # Data wasn't found, raise appropriate error
-        if key.isalpha():
+        if _can_int(key):
             # Episode number x was not found
             raise tvdb_seasonnotfound("Could not find season %s" % (repr(key)))
         else:
