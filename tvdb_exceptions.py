fbe29be186712c50e94e35317e5a3c40c9ef114d
dbr <dbr.onix@gmail.com>
2009-05-27 22:34:33 +0100

    Reformat exceptions (spacing)

--- tvdb_exceptions.py
+++ tvdb_exceptions.py
@@ -19,23 +19,28 @@ class tvdb_error(Exception):
     """An error with www.thetvdb.com (Cannot connect, for example)
     """
     pass
+
 class tvdb_userabort(Exception):
     """User aborted the interactive selection (via
     the q command, ^c etc)
     """
     pass
+
 class tvdb_shownotfound(Exception):
     """Show cannot be found on www.thetvdb.com (non-existant show)
     """
     pass
+
 class tvdb_seasonnotfound(Exception):
     """Season cannot be found on www.thetvdb.com
     """
     pass
+
 class tvdb_episodenotfound(Exception):
     """Episode cannot be found on www.thetvdb.com
     """
     pass
+
 class tvdb_attributenotfound(Exception):
     """Raised if an episode does not have the requested
     attribute (such as a episode name)
