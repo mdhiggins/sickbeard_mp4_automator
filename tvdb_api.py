2f6f760249dcc01df1d5a602f84a6411ea15fd39
dbr <dbr.onix@gmail.com>
2012-03-07 15:05:52 +1030

    Merge in \r sanitisation from Sick-Beard

--- tvdb_api.py
+++ tvdb_api.py
@@ -504,11 +504,13 @@ class Tvdb:
         """
         src = self._loadUrl(url)
         try:
-            return ElementTree.fromstring(src)
+            # TVDB doesn't sanitize \r (CR) from user input in some fields,
+            # remove it to avoid errors. Change from SickBeard, from will14m
+            return ElementTree.fromstring(src.rstrip("\r"))
         except SyntaxError:
             src = self._loadUrl(url, recache=True)
             try:
-                return ElementTree.fromstring(src)
+                return ElementTree.fromstring(src.rstrip("\r"))
             except SyntaxError, exceptionmsg:
                 errormsg = "There was an error with the XML retrieved from thetvdb.com:\n%s" % (
                     exceptionmsg
