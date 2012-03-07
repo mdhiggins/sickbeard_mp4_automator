f4acfe4c60ed93f27b1da9844c117eafc77242c1
dbr <dbr.onix@gmail.com>
2012-03-07 16:44:25 +1030

    Bump to 1.6.1

--- tvdb_exceptions.py
+++ tvdb_exceptions.py
@@ -9,7 +9,7 @@
 """
 
 __author__ = "dbr/Ben"
-__version__ = "1.6"
+__version__ = "1.6.1"
 
 __all__ = ["tvdb_error", "tvdb_userabort", "tvdb_shownotfound",
 "tvdb_seasonnotfound", "tvdb_episodenotfound", "tvdb_attributenotfound"]
