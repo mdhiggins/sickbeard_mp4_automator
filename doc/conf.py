ee93e3ccc500a40bc55782b7e4967aeeb3942c1b
Senko Rasic <senko.rasic@dobarkod.hr>
2012-03-11 09:17:08 +0100

    doc: updated copyright

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Video Converter'
-copyright = u'2011, Dobar Kod'
+copyright = u'2011,2012, Dobar Kod'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
