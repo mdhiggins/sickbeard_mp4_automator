8ccee2fa29d8f78cfb97b9f3fc73451f2a747ced
Michael Higgins <mdhiggins23@gmail.com>
2013-02-16 03:01:57 -0500

    updated postConversion to use new extensions-based check

--- postConversion.py
+++ postConversion.py
@@ -5,6 +5,7 @@ import urllib
 import ConfigParser
 from tvdb_mp4 import Tvdb_mp4
 from mkvtomp4 import MkvtoMp4
+from extensions import valid_output_extensions
 
 #Sickbeard API goodies
 config = ConfigParser.ConfigParser()
@@ -29,25 +30,21 @@ sickbeard_url = protocol + ip + ":" + port + "/api/" + api_key + "/"
 
 if len(sys.argv) > 4:
     path = str(sys.argv[1]).replace("\\","\\\\").replace("\\\\\\\\","\\\\")
+    extension = os.path.splitext(path)[1][1:]
     tvdb_id = int(sys.argv[3])
     season = int(sys.argv[4])
     episode  = int(sys.argv[5])
 
-    #Attempt to convert the file, if its already converted, say via sabToSickBeardwithConverter.py, it will just set the output file to the mp4
-    if path.endswith(".mp4"):
-        print "MP4 file detected, will not convert"
-        finalpath = path
-    else:
-        print "Converting file " + path
+    if extension not in valid_output_extensions:
         convert = MkvtoMp4(path, ffmpeg, ffprobe)
-        fullURL = sickbeard_url+"?cmd=show.refresh&tvdbid=" + str(tvdb_id)
+        path = convert.output
+        fullURL = sickbeard_url + "?cmd=show.refresh&tvdbid=" + str(tvdb_id)
         refresh = json.load(urllib.urlopen(fullURL))
         for item in refresh:
             print refresh[item]
-        finalpath = convert.output
         
     tagMp4 = Tvdb_mp4(tvdb_id, season, episode)
-    tagMp4.writeTags(finalpath)
-    
+    tagMp4.writeTags(path)
 else:
     print "Not enough command line arguments present " + str(len(sys.argv))
+    sys.exit()
