979147bf98bf7f08bcda483f4ec8137759afef8e
Michael Higgins <mdhiggins23@gmail.com>
2013-02-18 20:29:38 -0500

    defaults fix
    Renamed tvdb_mp4.ini to tvdb_mp4.ini.default so as to not constantly
    overwrite settings with each update.
    
    readSettings.py now includes the default values and will assume them
    when a value is not found or in the absence of a config file.

--- readSettings.py
+++ readSettings.py
@@ -4,29 +4,31 @@ import ConfigParser
 
 class ReadSettings:
     def __init__ (self, directory, filename):
-        config = ConfigParser.ConfigParser()
+        defaults = {'ip': 'localhost',
+                    'port': '8081',
+                    'ssl': "False",
+                    'api_key': '',
+                    'ffmpeg': 'ffmpeg.exe',
+                    'ffprobe': 'ffprobe.exe',
+                    'output_directory': '',
+                    'output_extension': 'mp4',
+                    'delete_original': "True"}
+        self.section = "TVDB_MP4"
+        config = ConfigParser.SafeConfigParser(defaults)
         configFile = os.path.join(directory, filename)
-        if not os.path.isfile(configFile):
-            print "Error: Config file not found"
-            sys.exit()
-        fp = open(configFile, "r")
-        config.readfp(fp)
-        fp.close()
-
-
-        self.ip = config.get("TVDB_MP4", "ip") #Server Address
-        self.port = config.get("TVDB_MP4", "port") #Server Port
-        self.api_key = config.get("TVDB_MP4", "api_key") #Sickbeard API key
-        self.ffmpeg = config.get("TVDB_MP4", "ffmpeg").replace("\\","\\\\").replace("\\\\\\\\","\\\\") #Location of FFMPEG.exe
-        self.ffprobe = config.get("TVDB_MP4", "ffprobe").replace("\\","\\\\").replace("\\\\\\\\","\\\\") #Location of FFPROBE.exe
-        self.output_dir = config.get("TVDB_MP4", "output_directory").replace("\\","\\\\").replace("\\\\\\\\","\\\\") #Output directory
-        self.output_extension = config.get("TVDB_MP4", "output_extension") #Output extension
-        self.delete = config.getboolean("TVDB_MP4", "delete_original") #Delete original file
-        
-        #SSL
-        self.protocol = "http://"
-        if config.getboolean("TVDB_MP4", "ssl"):
-            self.protocol = "https://"
+        if os.path.isfile(configFile):
+            fp = open(configFile, "r")
+            config.readfp(fp)
+            fp.close()
+        else:
+            print "Error: Config file not found, using default values"
+        if not config.has_section(self.section):
+            self.section = "DEFAULT"
+        self.ffmpeg = config.get(self.section, "ffmpeg").replace("\\","\\\\").replace("\\\\\\\\","\\\\") #Location of FFMPEG.exe
+        self.ffprobe = config.get(self.section, "ffprobe").replace("\\","\\\\").replace("\\\\\\\\","\\\\") #Location of FFPROBE.exe
+        self.output_dir = config.get(self.section, "output_directory").replace("\\","\\\\").replace("\\\\\\\\","\\\\") #Output directory
+        self.output_extension = config.get(self.section, "output_extension") #Output extension
+        self.delete = config.getboolean(self.section, "delete_original") #Delete original file
 
         if self.output_dir == "" and self.delete is False:
             print "Error - you must specify an alternate output directory if you aren't going to delete the original file"
@@ -37,7 +39,15 @@ class ReadSettings:
         else:
             if not os.path.isdir(self.output_dir):
                 os.makedirs(self.output_dir)
-            
+        self.config = config
     def getRefreshURL (self, tvdb_id):
-        sickbeard_url = self.protocol + self.ip + ":" + self.port + "/api/" + self.api_key + "/?cmd=show.refresh&tvdbid=" + str(tvdb_id)
+        config = self.config
+        #SSL
+        protocol = "http://"
+        if config.getboolean(self.section, "ssl"):
+            protocol = "https://"
+        ip = config.get(self.section, "ip") #Server Address
+        port = config.get(self.section, "port") #Server Port
+        api_key = config.get(self.section, "api_key") #Sickbeard API key
+        sickbeard_url = protocol + ip + ":" + port + "/api/" + api_key + "/?cmd=show.refresh&tvdbid=" + str(tvdb_id)
         return sickbeard_url
