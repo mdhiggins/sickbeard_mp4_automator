a43fb495d10aaca9009601e2d1b2c1eccd7820e5
Michael Higgins <mdhiggins23@gmail.com>
2013-02-23 10:17:07 -0500

    qtfaststart subtree merging
    giving this a try

---
+++ qtfaststart/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import qtfaststart
+
+long_description = open('README.txt').read()
+
+setup(
+    name='qtfaststart',
+    version=qtfaststart.VERSION,
+    description='Quicktime atom positioning in Python for fast streaming.',
+    long_description=long_description,
+    author='Daniel G. Taylor',
+    author_email='dan@programmer-art.org',
+    url='https://github.com/gtaylor/qtfaststart',
+    license='MIT License',
+    platforms=["any"],
+    provides=['qtfaststart'],
+    packages=[
+        'qtfaststart',
+    ],
+    scripts=['bin/qtfaststart'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Video :: Conversion',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
