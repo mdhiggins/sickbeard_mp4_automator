dd50f76463e2d7e95d00ea6c72c8aff6e1b5d1b7
Michael Higgins <mdhiggins23@gmail.com>
2013-02-22 22:02:45 -0500

    tvdb_mp4.py cleanup
    Mostly syntax cleanup; removed some unneeded imports updated the
    extension checking method with new logic

--- tvdb_mp4.py
+++ tvdb_mp4.py
@@ -1,42 +1,41 @@
 import os
-import pipes
 import sys
 import urllib
 import StringIO
 import tempfile
 import time
-import tvdb_api
-from tvdb_api import tvdb_api
+from tvdb_api.tvdb_api import Tvdb
 from mutagen.mp4 import MP4, MP4Cover
 from extensions import valid_output_extensions
+
 
 class Tvdb_mp4:
     def __init__(self, show, season, episode):
         attempts = 0
         while attempts < 3:
             try:
-                self.tvdb_show = tvdb_api.Tvdb(interactive=False, cache=False, banners=True, actors=True, forceConnect=True)
+                self.tvdb_show = Tvdb(interactive=False, cache=False, banners=True, actors=True, forceConnect=True)
                 self.show = show
                 self.season = season
                 self.episode = episode
                 self.HD = None
-                
+
                 #Gather information from theTVDB
                 self.showdata = self.tvdb_show[self.show]
                 self.seasondata = self.showdata[self.season]
                 self.episodedata = self.seasondata[self.episode]
                 #episodedata = self.tvdb_show[self.show][self.season][self.episode]
-                
+
                 self.show = self.showdata['seriesname']
                 self.genre = self.showdata['genre']
                 self.network = self.showdata['network']
-                
+
                 self.title = self.episodedata['episodename']
                 self.description = self.episodedata['overview']
                 self.airdate = self.episodedata['firstaired']
                 self.director = self.episodedata['director']
                 self.writer = self.episodedata['writer']
-                
+
                 #Generate XML tags for Actors/Writers/Directors
                 self.xml = self.xmlTags()
                 break
@@ -52,40 +51,38 @@ class Tvdb_mp4:
         if ext not in valid_output_extensions:
             print "Error: File is not the correct format"
             sys.exit()
-        
         try:
             MP4(mp4Path).delete()
         except IOError:
             print "Unable to clear original tags"
-        
-        ##Artwork will also require additional processing -- see artwork
-        path = self.getArtwork()
-                
-        video=MP4(mp4Path)
-        video["tvsh"] = self.show #TV show title
-        video["\xa9nam"] = self.title #Video title
-        video["tven"] = self.title #Episode title
-        video["desc"] = self.description #Short description
-        video["ldes"] = self.description #Long description (same a short for tv)
-        video["tvnn"] = self.network #Network
+
+        video = MP4(mp4Path)
+        video["tvsh"] = self.show  # TV show title
+        video["\xa9nam"] = self.title  # Video title
+        video["tven"] = self.title  # Episode title
+        video["desc"] = self.description  # Short description
+        video["ldes"] = self.description  # Long description (same a short for tv)
+        video["tvnn"] = self.network  # Network
         if self.airdate != "0000-00-00":
-            video["\xa9day"] = self.airdate #Airdate
-        video["tvsn"] = [self.season] #Season number
-        video["\xa9alb"] = self.show + ", Season " + str(self.season)
-        video["tves"] = [self.episode] #Episode number
-        video["trkn"] = [(int(self.episode), len(self.seasondata))] #Episode number iTunes
-        video["stik"] = [10] #TV show iTunes category
+            video["\xa9day"] = self.airdate  # Airdate
+        video["tvsn"] = [self.season]  # Season number
+        video["\xa9alb"] = self.show + ", Season " + str(self.season)  # iTunes Album as Season
+        video["tves"] = [self.episode]  # Episode number
+        video["trkn"] = [(int(self.episode), len(self.seasondata))]  # Episode number iTunes
+        video["stik"] = [10]  # TV show iTunes category
         if self.HD is not None:
             video["hdvd"] = self.HD
         if self.genre != None:
-            video["\xa9gen"] = self.genre.replace('|',',')[1:-1] #Genre(s)
-        video["----:com.apple.iTunes:iTunMOVI"] = self.xml #XML - see xmlTags method
+            video["\xa9gen"] = self.genre.replace('|', ',')[1:-1]  # Genre(s)
+        video["----:com.apple.iTunes:iTunMOVI"] = self.xml  # XML - see xmlTags method
+
+        path = self.getArtwork()
         if path is not None:
             cover = open(path, 'rb').read()
             if path.endswith('png'):
-                video["covr"] = [MP4Cover(cover, MP4Cover.FORMAT_PNG)] #png poster
+                video["covr"] = [MP4Cover(cover, MP4Cover.FORMAT_PNG)]  # png poster
             else:
-                video["covr"] = [MP4Cover(cover, MP4Cover.FORMAT_JPEG)] #jpeg poster
+                video["covr"] = [MP4Cover(cover, MP4Cover.FORMAT_JPEG)]  # jpeg poster
         video["\xa9too"] = "Sickbeard MP4 Automator MDH"
         video.pprint()
         attempts = 0
@@ -102,7 +99,7 @@ class Tvdb_mp4:
                 time.sleep(5)
                 attempts += 1
     #end writeTags
-    
+
     def setHD(self, width, height):
         if width >= 1920 or height >= 1080:
             self.HD = [2]
@@ -110,7 +107,7 @@ class Tvdb_mp4:
             self.HD = [1]
         else:
             self.HD = [0]
-            
+
     def xmlTags(self):
         #constants
         header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><dict>\n"
@@ -119,18 +116,18 @@ class Tvdb_mp4:
         directorheader = "<key>directors</key><array>\n"
         subfooter = "</array>\n"
         footer = "</dict></plist>\n"
-        
+
         output = StringIO.StringIO()
         output.write(header)
         output.write(castheader)
-        
-        count = 0
+
         #Write Actors
-        while (count < len(self.showdata['_actors'])-1):
+        count = 0
+        while (count < len(self.showdata['_actors']) - 1):
             name = self.showdata['_actors'][count]['name']
             if name != None:
                 output.write("<dict><key>name</key><string>" + name.encode('ascii', errors='ignore') + "</string></dict>\n")
-            count = count+1
+            count = count + 1
         output.write(subfooter)
         #write Screenwriter
         if self.writer != None:
@@ -150,7 +147,7 @@ class Tvdb_mp4:
         return output.getvalue()
         output.close()
     #end xmlTags
-    
+
     def getArtwork(self):
         #Pulls down all the poster metadata for the correct season and sorts them into the Poster object
         posters = posterCollection()
@@ -170,18 +167,21 @@ class Tvdb_mp4:
     #end artwork
 #end tvdb_mp4
 
+
 class Poster:
     #simple container for all the poster parameters needed
-    def __init__(self, rating = 0, ratingcount = 0, bannerpath = ""):
+    def __init__(self, rating=0, ratingcount=0, bannerpath=""):
         self.rating = rating
         self.bannerpath = bannerpath
         self.ratingcount = ratingcount
         #self.language
     #end poster
 
+
 class posterCollection:
     def __init__(self):
         self.posters = []
+
     def topPoster(self):
         #Determines which poster has the highest rating, returns the Poster object
         top = None
@@ -193,23 +193,24 @@ class posterCollection:
             elif poster.rating == top.rating and poster.ratingcount > top.ratingcount:
                 top = poster
         return top
+
     def addPoster(self, inputPoster):
         self.posters.append(inputPoster)
-    
+
+
 def main():
-    if len(sys.argv) > 3:
-        tvdb_instance = tvdb_api.Tvdb(interactive=False, cache=False, banners=True, actors=True)
-        mp4 = str(sys.argv[1]).replace("\\","\\\\").replace("\\\\\\\\","\\\\")
-        tvdb_id = str(sys.argv[3])
+    if len(sys.argv) > 4:
+        mp4 = str(sys.argv[1]).replace("\\", "\\\\").replace("\\\\\\\\", "\\\\")
+        tvdb_id = str(sys.argv[2])
         if tvdb_id.isdigit():
             tvdb_id = int(tvdb_id)
-        tvdb_season = int(sys.argv[4])
-        tvdb_episode = int(sys.argv[5])
+        tvdb_season = int(sys.argv[3])
+        tvdb_episode = int(sys.argv[4])
         tvdb_mp4_instance = Tvdb_mp4(show=tvdb_id, season=tvdb_season, episode=tvdb_episode)
-        if mp4.endswith(".mp4") or mp4.endswith(".m4v"):
+        if os.path.splitext(mp4)[1][1:] in valid_output_extensions:
             tvdb_mp4_instance.writeTags(mp4)
         else:
             print "Wrong file type (must be .mp4 or .m4v)"
 
 if __name__ == '__main__':
-    main()    
+    main()
