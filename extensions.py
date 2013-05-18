e761786dfa87b2d5b72512062225ff04f8c41a1b
Michael Higgins <mdhiggins23@gmail.com>
2013-05-18 13:23:53 -0400

    whitespace

--- extensions.py
+++ extensions.py
@@ -2,4 +2,4 @@ valid_input_extensions = ['mkv', 'avi']
 valid_output_extensions = ['mp4', 'm4v']
 valid_audio_codecs = ['aac', 'ac3', 'dts']
 bad_subtitle_codecs = ['pgssub', 'dvdsub']
-tmdb_api_key = "45e408d2851e968e6e4d0353ce621c66" 
+tmdb_api_key = "45e408d2851e968e6e4d0353ce621c66"
