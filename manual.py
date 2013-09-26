636108a264af87d4185ee48213e85f3db004d610
Michael Higgins <mdhiggins23@gmail.com>
2013-09-26 19:47:08 -0400

    major manual.py overhaul part 2
    - Cleaned up a large amount of unneeded code from the original script
    and the first iteration of the overall
    - Added guessing as a default part of the file processing, with the
    option to use guess or specific the information manually
    - Improved command line functionality, you may now specify all options,
    a directory, or a file name and the script will process everything
    accordingly.
    - Consolidated batch operations into a -silent switch for command line
    purposes
    - The -silent switch can be run standalone. After a file or directory is
    entered into the prompt the script will proceed without any
    intervention.
    - If an unexpected error occurs, the script should proceed to the next
    file in a batch operation
    - Added option to skip file in non-silent batch operation
    - Added option to convert file without tagging

--- manual.py
+++ manual.py
@@ -2,260 +2,229 @@
 #
 import sys
 import os
-from readSettings import ReadSettings
 import guessit
+from readSettings import ReadSettings
 from tvdb_mp4 import Tvdb_mp4
 from tmdb_mp4 import tmdb_mp4
 from mkvtomp4 import MkvtoMp4
 from tvdb_api import tvdb_api
 from tmdb_api import tmdb
-from extensions import valid_input_extensions, valid_output_extensions
 from extensions import tmdb_api_key
-import shutil
 
 settings = ReadSettings(os.path.dirname(sys.argv[0]), "autoProcess.ini")
 
+if settings.output_dir is not None:
+    import shutil
+
 def mediatype():
-	print "Select media type:"
-	print "1. Movie (via IMDB ID)"
-	print "2. Movie (via TMDB ID)"
-	print "3. TV (via TVDB ID)"
-	print "4. GuessIT!"
-	result = raw_input("#: ")
-	if 0 < int(result) < 5:
-		return int(result)
-	else:
-		print "Invalid selection"
-		return mediatype()
+    print "Select media type:"
+    print "1. Movie (via IMDB ID)"
+    print "2. Movie (via TMDB ID)"
+    print "3. TV"
+    print "4. Convert without tagging"
+    print "5. Skip file"
+    result = raw_input("#: ")
+    if 0 < int(result) < 6:
+        return int(result)
+    else:
+        print "Invalid selection"
+        return mediatype()
+
+
+def getValue(prompt, num=False):
+    print prompt + ":"
+    value = raw_input("#: ")
+    if num is True and value.isdigit() is False:
+        print "Must be a numerical value"
+        return getValue(prompt, num)
+    else:
+        return str(value)
+
+
+def getYesNo():
+    yes = ['y', 'yes', 'true', '1']
+    no = ['n', 'no', 'false', '0']
+    data = raw_input("# [y/n]: ")
+    if data.lower() in yes:
+        return True
+    elif data.lower() in no:
+        return False
+    else:
+        print "Invalid selection"
+        return getYesNo()
+
+
+def getinfo(fileName=None, silent=False, guess=True):
+    # Try to guess the file is guessing is enabled
+    if fileName is not None and guess:
+        tagdata = guessInfo(fileName)
+        # If the guess returned something, proceed
+        if tagdata is not None:
+            # If script is running in silent mode, skip confirmation, otherwise confirm.
+            if silent:
+                return tagdata
+            else:
+                print "Proceed using guessed identification from filename?"
+                if getYesNo() and guess:
+                    return tagdata
+    if silent is False:
+        m_type = mediatype()
+        if m_type is 3:
+            tvdbid = getValue("Enter TVDB Series ID", True)
+            season = getValue("Enter Season Number", True)
+            episode = getValue("Enter Episode Number", True)
+            return m_type, tvdbid, season, episode
+        elif m_type is 1:
+            imdbid = getValue("Enter IMDB ID")
+            return m_type, imdbid
+        elif m_type is 2:
+            tmdbid = getValue("Enter TMDB ID", True)
+            return m_type, tmdbid
+        elif m_type is 4:
+            return None
+        elif m_type is 5:
+            return False
+    else:
+        # Probably add a setting in the future to control this behavior
+        return None
+
 
 def guessInfo(fileName):
-	guess = guessit.guess_video_info(fileName)
-	if guess["type"] == "movie":
-		return tmdbInfo(guess)
-	else:
-		return tvdbInfo(guess)
+    guess = guessit.guess_video_info(fileName)
+    try:
+        if guess["type"] == "movie":
+            return tmdbInfo(guess)
+        else:
+            return tvdbInfo(guess)
+    except:
+        return None
+
 
 def tmdbInfo(guessData):
-	tmdb.configure(tmdb_api_key)
-	movies = tmdb.Movies(guessData["title"])
-	for movie in movies.iter_results():
-		#Identify the first movie in the collection that matches exactly the movie title
-		foundname = ''.join(e for e in movie["title"] if e.isalnum())
-		origname = ''.join(e for e in guessData["title"] if e.isalnum())
-		#origname = origname.replace('&', 'and')
-		if foundname.lower() == origname.lower():
-			print "Matched movie title as: %s %s" % (movie["title"], movie["release_date"])
-			movie = tmdb.Movie(movie["id"])
-			break
-	if isinstance(movie, dict):
-		tmdbid = movie["id"]
-	else:
-		tmdbid = movie.get_id()
-	return "movie", tmdbid
+    tmdb.configure(tmdb_api_key)
+    movies = tmdb.Movies(guessData["title"])
+    for movie in movies.iter_results():
+        #Identify the first movie in the collection that matches exactly the movie title
+        foundname = ''.join(e for e in movie["title"] if e.isalnum())
+        origname = ''.join(e for e in guessData["title"] if e.isalnum())
+        #origname = origname.replace('&', 'and')
+        if foundname.lower() == origname.lower():
+            print "Matched movie title as: %s %s" % (movie["title"], movie["release_date"])
+            movie = tmdb.Movie(movie["id"])
+            break
+    if isinstance(movie, dict):
+        tmdbid = movie["id"]
+    else:
+        tmdbid = movie.get_id()
+    return 2, tmdbid
 
 
 def tvdbInfo(guessData):
-	seasonNum = guessData["season"]
-	episodeNum = guessData["episodeNumber"]
-	series = guessData["series"]
-	print "Guessing %s TVDB info" % (series)
-	t = tvdb_api.Tvdb()
-	show = t[series]
-	return "tv", show['id'], seasonNum, episodeNum
-
-def convertFile(path):
-	convert = MkvtoMp4(path, FFMPEG_PATH=settings.ffmpeg, FFPROBE_PATH=settings.ffprobe, delete=settings.delete, output_extension=settings.output_extension, relocate_moov=settings.relocate_moov, iOS=settings.iOS, awl=settings.awl, swl=settings.swl, adl=settings.adl, sdl=settings.sdl, audio_codec=settings.acodec, processMP4=settings.processMP4)
-	return convert
-
-def tagFile(tagInfo, fileInfo, path):
-	if fileInfo is not None:
-		tagInfo.setHD(fileInfo.width, fileInfo.height)
-		path = fileInfo.output
-		tagInfo.writeTags(path)
-		if settings.relocate_moov:
-			fileInfo.QTFS()
-
-def moveFile(path):
-	output = os.path.join(settings.output_dir, os.path.split(path)[1])
-	shutil.move(path, output)
-	print "Moved File"
-
-def getIMDBId():
-	print "Enter IMDB ID:"
-	imdbid = raw_input("#: ")
-	return imdbid
+    seasonNum = guessData["season"]
+    episodeNum = guessData["episodeNumber"]
+    series = guessData["series"]
+    print "Guessing %s TVDB info" % (series)
+    t = tvdb_api.Tvdb()
+    show = t[series]
+    return 3, show['id'], seasonNum, episodeNum
 
 
-def getTMDBId():
-	print "Enter TMDB ID:"
-	tmdbid = raw_input("#: ")
-	return tmdbid
+def processFile(path, tagdata, output_dir=None):
+    if tagdata is False:
+        return # This means the user has elected to skip the file
+    elif tagdata is None:
+        tagmp4 = None # No tag data specified but convert the file anyway
+    elif tagdata[0] is 1:
+        imdbid = tagdata[1]
+        tagmp4 = tmdb_mp4(imdbid)
+        print "Processing %s" % (tagmp4.title)
+    elif tagdata[0] is 2:
+        tmdbid = tagdata[1]
+        tagmp4 = tmdb_mp4(tmdbid, True)
+        print "Processing %s" % (tagmp4.title)
+    elif tagdata[0] is 3:
+        tvdbid = int(tagdata[1])
+        season = int(tagdata[2])
+        episode = int(tagdata[3])
+        tagmp4 = Tvdb_mp4(tvdbid, season, episode)
+        print "Processing %s Season %s Episode %s - %s" % (tagmp4.show, str(tagmp4.season), str(tagmp4.episode), tagmp4.title)
+    convert = MkvtoMp4(path, 
+                    FFMPEG_PATH=settings.ffmpeg, 
+                    FFPROBE_PATH=settings.ffprobe, 
+                    delete=settings.delete, 
+                    output_extension=settings.output_extension, 
+                    relocate_moov=settings.relocate_moov, 
+                    iOS=settings.iOS, 
+                    awl=settings.awl, 
+                    swl=settings.swl, 
+                    adl=settings.adl, 
+                    sdl=settings.sdl, 
+                    audio_codec=settings.acodec, 
+                    processMP4=settings.processMP4, 
+                    reportProgress=True)
+    if convert.output is not None:
+        if tagmp4 is not None:
+            tagmp4.setHD(convert.width, convert.height)
+            tagmp4.writeTags(convert.output)
+        if settings.relocate_moov:
+            convert.QTFS()
+        if output_dir is not None:
+            output = os.path.join(settings.output_dir, os.path.split(path)[1])
+            try:
+                shutil.move(path, output)
+                print "File %s moved to %s" % (path, output)
+            except:
+                print "Unable to move file %s to %s" % (path, output)
 
-
-def getTVDBId():
-	print "Enter TVDB Series ID:"
-	tvdbid = raw_input("#: ")
-	return tvdbid
-
-
-def getSeason():
-	print "Enter Season Number:"
-	season = raw_input("#: ")
-	return season
-
-
-def getEpisode():
-	print "Enter Episode Number:"
-	episode = raw_input("#: ")
-	return episode
-
-
-def getinfo():
-	m_type = mediatype()
-	if m_type is 3:
-		tvdbid = getTVDBId()
-		season = getSeason()
-		episode = getEpisode()
-		return m_type, tvdbid, season, episode
-	elif m_type is 4:
-		return [m_type]
-		#guess = guessit.guess_movie_info(file_name)
-	elif m_type is 1:
-		imdbid = getIMDBId()
-		return m_type, imdbid
-	elif m_type is 2:
-		tmdbid = getTMDBId()
-		return m_type, tmdbid
-
-
-def stageFile(path, inputs):
-	if inputs is None:
-		fileName = os.path.basename(path)
-		guess = guessInfo(fileName)
-		if guess[0] == "movie":
-			tagmp4 = tmdb_mp4(guess[1], True)
-			print "Processing %s" % (tagmp4.title)
-		elif guess[0] == "tv":
-			tagmp4 = Tvdb_mp4(int(guess[1]), int(guess[2]), int(guess[3]))
-			print "Processing %s Season %s Episode %s - %s" %(tagmp4.show, str(tagmp4.season), str(tagmp4.episode), tagmp4.title)
-	elif inputs[2] is not None:
-		tvdbid = inputs[2]
-		season = inputs[3]
-		episode = inputs[4]
-		tagmp4 = Tvdb_mp4(tvdbid, season, episode)
-		print "Processing %s Season %s Episode %s - %s" %(tagmp4.show, str(tagmp4.season), str(tagmp4.episode), tagmp4.title)
-	elif inputs[0] is not None:
-		imdbid = inputs[0]
-		tagmp4 = tmdb_mp4(imdbid)
-		print "Processing %s" % (tagmp4.title)
-	elif inputs[1] is not None:
-		tmdbid = inputs[1]
-		tagmp4 = tmdb_mp4(tmdbid, True)
-		print "Processing %s" % (tagmp4.title)
-	else:
-		print "Invalid command line input"
-		return
-	return tagmp4
-    #elif len(sys.argv) == 2:
-    #    path = str(sys.argv[1]).replace("\\", "\\\\").replace("\\\\\\\\", "\\\\")
-    #    getinfo()
-
-def isValidInput(fileName):
-	return os.path.splitext(fileName)[1][1:] in valid_input_extensions
-
-def isValidOutput(fileName):
-	return os.path.splitext(fileName)[1][1:] in valid_output_extensions
-
-def iteratePath(path, inputs):
-	for r,d,f in os.walk(path):
-		for file in f:
-			filepath = os.path.join(r, file)
-			os.chmod(filepath, 0777)
-			processFile(filepath, inputs)
-
-def convertArguments(args):
-	if args[2] == '-tv':
-		tvdbid = int(args[3])
-		season = int(args[4])
-		episode = int(args[5])
-		inputs = (None, None, tvdbid, season, episode)
-	elif args[2] == '-m':
-		imdbid = args[3]
-		inputs = (imdbid, None, None, None, None)
-	elif args[2] == '-tmdb':
-		tmdbid = args[3]
-		inputs = (None, tmdbid, None, None, None)
-	elif args[2] == '-guess':
-		inputs = None
-	return inputs
-
-# Is there any way to condense this function with convertArguments?
-def convertInput(results):
-	if results[0] is 3:
-		tvdbid = int(results[1])
-		season = int(results[2])
-		episode = int(results[3])
-		inputs = (None, None, tvdbid, season, episode)
-	elif results[0] is 1:
-		imdbid = results[1]
-		inputs = (imdbid, None, None, None, None)
-	elif results[0] is 2:
-		tmdbid = results[1]
-		inputs = (None, tmdbid, None, None, None)
-	elif results[0] is 4:
-		inputs = None
-	return inputs
-
-def processFile(filepath, inputs):
-	fileName = os.path.basename(filepath)
-	if isValidInput(fileName):
-		tagInfo = stageFile(filepath, inputs)
-		fileInfo = convertFile(filepath)
-		tagFile(tagInfo, fileInfo, None)
-		if (settings.output_dir is not None) and ("-nomove" not in sys.argv):
-			moveFile(fileInfo.output)
-		print "--------------------------------"
-	elif isValidOutput(fileName):
-		tagInfo = stageFile(filepath, inputs)
-		tagFile(tagInfo, None, filepath)
-		if (settings.output_dir is not None) and ("-nomove" not in sys.argv):
-			moveFile(filepath)
-		print "--------------------------------"
+def walkDir(dir, silent=False, output_dir=None):
+    for r,d,f in os.walk(dir):
+        for file in f:
+            filepath = os.path.join(r, file)
+            print "Processing file %s" % (filepath)
+            try:
+                tagdata = getinfo(filepath, silent)
+                processFile(filepath, tagdata, output_dir)
+            except Exception as e:
+                print "An unexpected error occured, processing of this file has failed"
+                print str(e)
 
 def main():
-	if len(sys.argv) > 2:
-		path = str(sys.argv[1])
-		inputs = convertArguments(sys.argv)
-		if sys.argv[2] == "-guess":
-			if os.path.isdir(path):
-				iteratePath(path, inputs)
-			elif os.path.isfile(path):
-				os.chmod(path, 0777)
-				processFile(path, inputs)
-		elif os.path.isdir(path):
-			print "Please specify a file or use the -guess flag"
-		else:
-			os.chmod(path, 0777)
-			processFile(path, inputs)
-	else:
-		print "Enter path to file:"
-		path = raw_input("#: ")
-		if path.startswith('"') and path.endswith('"'):
-			path = path[1:-1]
-		result = getinfo()
-		inputs = convertInput(result)
-		os.chmod(path, 0777)
-		processFile(path, inputs)
+    silent = True if '-silent' in sys.argv else False
+    output_dir = settings.output_dir if settings.output_dir is not None and '-nomove' not in sys.argv else None
 
-#		elif result[0] is 4:
-#			fileName = os.path.basename(path)
-#			guess = guessInfo(fileName)
-#			if guess[0] == "movie":
-#				tagmp4 = tmdb_mp4(guess[1], True)
-#				print "Processing %s" % (tagmp4.title)
-#			elif guess[0] == "tv":
-#				tagmp4 = Tvdb_mp4(int(guess[1]), int(guess[2]), int(guess[3]))
-#				print "Processing %s Season %s Episode %s - %s" %(tagmp4.show, str(tagmp4.season), str(tagmp4.episode), tagmp4.title)
+    if len(sys.argv) > 2:
+        path = str(sys.argv[1])
+        # Gather info from command line
+        if os.path.isdir(path):
+            walkDir(path, silent, output_dir)
+        elif os.path.isfile(path):
+            if sys.argv[2] == '-tv':
+                tvdbid = int(sys.argv[3])
+                season = int(sys.argv[4])
+                episode = int(sys.argv[5])
+                tagdata = [3, tvdbid, season, episode]
+            elif sys.argv[2] == '-m':
+                imdbid = sys.argv[3]
+                tagdata = [1, imdbid]
+            elif sys.argv[2] == '-tmdb':
+                tmdbid = sys.argv[3]
+                tagdata = [2, tmdbid]
+            else:
+                tagdata = getinfo(path, silent)
+            processFile(path, tagdata, output_dir)
+        else:
+            print "Invalid command line input"
+    # Ask for the info
+    else:
+        print "Enter path to file:"
+        path = raw_input("#: ")
+        if path.startswith('"') and path.endswith('"'):
+            path = path[1:-1]
+        if os.path.isdir(path):
+            walkDir(path, silent, output_dir)
+        else:
+            tagdata = getinfo(path, silent=silent)
+            processFile(path, tagdata, output_dir)
 
 if __name__ == '__main__':
-	main()
+    main()
