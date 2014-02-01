fa47ccb0a3a9ee013633063d4e7ac8d71bbf8c6b
Michael Higgins <mdhiggins23@gmail.com>
2014-01-31 21:04:21 -0500

    revert

--- converter/avcodecs.py
+++ converter/avcodecs.py
@@ -159,7 +159,6 @@ class SubtitleCodec(BaseCodec):
         if 'codec' in safe:
             optlist.extend(['-c:s:' + stream, self.ffmpeg_codec_name])
         if 'path' in safe:
-            optlist.extend(["-sub_charenc", "UTF-8"])
             optlist.extend(['-i', str(safe['path'])])
         if 'default' in safe:
             optlist.extend(['-metadata:s:s:' + stream, "disposition:default=" + str(safe['default'])])
