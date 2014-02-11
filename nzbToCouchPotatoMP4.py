fd823defa92dfe4a81e74dbc02e6bcdfe6b7984a
Michael Higgins <mdhiggins23@gmail.com>
2014-02-11 17:55:42 -0500

    removed legacy CP code

--- nzbToCouchPotatoMP4.py
+++ nzbToCouchPotatoMP4.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import autoProcessMovie
-import guessit
 from tmdb_api import tmdb
 from tmdb_mp4 import tmdb_mp4
 from readSettings import ReadSettings
@@ -13,47 +12,8 @@ from extensions import tmdb_api_key
 print "nzbToCouchPotato MP4 edition"
 
 
-def FILEtoIMDB(file_name):
-    """
-    Added function by nctiggy. This executes if the nzb does not have the IMDB id appended to the name
-    This does capture all of the movies info not just the IMDB id
-    """
-
-    print "CouchPotatoServer did not append the IMDB id to the nzb, guessing instead"
-
-    # Guessing at the name of the movie using the filename
-    movie_info = guessit.guess_movie_info(file_name)
-
-    #configuring tmdb to use the supplied api key
-    tmdb.configure(tmdb_api_key)
-    print "Guessed movie title as: %s" % (movie_info["title"])
-
-    #Creating a collection of movies from tmdb for the guess movie title
-    movies = tmdb.Movies(movie_info["title"])
-
-    #parse through all movies found
-    for movie in movies.iter_results():
-        #Identify the first movie in the collection that matches exactly the movie title
-        foundname = ''.join(e for e in movie["title"] if e.isalnum())
-        origname = ''.join(e for e in movie_info["title"] if e.isalnum())
-        if foundname.lower() == origname.lower():
-            print "Matched movie title as: %s %s" % (movie["title"], movie["release_date"])
-            movie = tmdb.Movie(movie["id"])
-            break
-    #return the imdb id of the movie identified
-    return movie.get_imdb_id()
-
-
-def NZBtoIMDB(nzbName):
-    nzbName = str(nzbName)
-    a = nzbName.find('.cp(tt') + 4
-    b = nzbName[a:].find(')') + a
-    imdbid = nzbName[a:b]
-    return imdbid
-
 if len(sys.argv) > 3:
     settings = ReadSettings(os.path.dirname(sys.argv[0]), "autoProcess.ini")
-    imdb_id = NZBtoIMDB(sys.argv[2])
     converter = MkvtoMp4(settings)
     path = str(sys.argv[1])
     moviefile = None
@@ -63,31 +23,7 @@ if len(sys.argv) > 3:
             inputfile = os.path.join(r, files)
             
             if MkvtoMp4(settings).validSource(inputfile):
-                size = os.path.getsize(inputfile)
-                if size > maxsize:
-                    moviefile = inputfile
-                    maxsize = size
-
-    if moviefile:
-        output = converter.process(moviefile)
-        # Tag with metadata
-        if settings.tagfile:
-            if imdb_id == "":
-                try:
-                    print "Going to guess the following files info: %s" % (sys.argv[2])
-                    imdb_id = FILEtoIMDB(os.path.basename(sys.argv[2]))
-                except:
-                    print "Unable to accurately identify movie file %s" % (moviefile)
-            print "IMDB ID is: %s" % (imdb_id)
-            try:
-                imdbmp4 = tmdb_mp4(imdb_id)
-                imdbmp4.setHD(output['x'], output['y'])
-                imdbmp4.writeTags(output['output'])
-                converter.QTFS(output['output'])
-            except AttributeError:
-                print "Unable to tag file, Couch Potato probably screwed up passing the IMDB ID"
-        # Copy to additional locations
-        converter.replicate(output['output'])
+                converter.process(moviefile)
 
 
 # SABnzbd
