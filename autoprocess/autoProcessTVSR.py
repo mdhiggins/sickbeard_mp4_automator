ba73dc341c26cde783720722317cd159d4617a47
Michael Higgins <mdhiggins23@gmail.com>
2015-03-04 16:21:17 -0500

    comments cleaned

--- autoprocess/autoProcessTVSR.py
+++ autoprocess/autoProcessTVSR.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
 
-# Author: Nic Wolfe <nic@wolfeden.ca>
-# URL: http://code.google.com/p/sickbeard/
 #
-# This file is part of SickRage.
+# This file is adapted from the autoProcessTV file included with SickRage.
 #
-# SickRage is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# SickRage is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import with_statement
 
