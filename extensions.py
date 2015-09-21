0388b66ab9bb2f430480db0800012d3e964259a3
michael <mdhiggins23@gmail.com>
2015-09-21 17:55:10 -0400

    mov support

--- extensions.py
+++ extensions.py
@@ -1,4 +1,4 @@
-valid_input_extensions = ['mkv', 'avi', 'ts']
+valid_input_extensions = ['mkv', 'avi', 'ts', 'mov']
 valid_output_extensions = ['mp4', 'm4v']
 valid_audio_codecs = ['aac', 'ac3', 'dts']
 valid_poster_extensions = ['jpg', 'png']
@@ -9,6 +9,6 @@ tmdb_api_key = "45e408d2851e968e6e4d0353ce621c66"
 valid_internal_subcodecs = ['mov_text']
 valid_external_subcodecs = ['srt', 'webvtt']
 subtitle_codec_extensions = { 'srt' : 'srt',
-							  'webvtt': 'vtt' }
+                              'webvtt': 'vtt' }
 bad_post_files = ['resources', '.DS_Store']
 bad_post_extensions = ['.txt', '.log', '.pyc']
