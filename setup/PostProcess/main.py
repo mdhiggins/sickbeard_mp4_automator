491d29aedd72403825cc6b56ffd7fa134578280f
Michael Higgins <mdhiggins23@gmail.com>
2015-10-22 15:08:54 -0400

    couchpotato change
    changed how the couchpotato post processing script functions back to the
    previous style of executing the script via popen. Was running into
    ImportError issues under windows when CP wasn't being run from source
    and the packages installed to python (babelfish, qtfaststart, etc)
    weren't accessible to the script because of the different environment.

--- setup/PostProcess/main.py
+++ setup/PostProcess/main.py
@@ -2,8 +2,8 @@ from couchpotato.core.event import addEvent
 from couchpotato.core.logger import CPLog
 from couchpotato.core.plugins.base import Plugin
 import traceback
-import sys
 import os
+import subprocess
 
 log = CPLog(__name__)
 
@@ -21,19 +21,7 @@ class PostProcess(Plugin):
 
         log.info('MP4 Automator - Post processing script initialized')
 
-        sys.path.append(path)
-        try:
-            from readSettings import ReadSettings
-            from mkvtomp4 import MkvtoMp4
-            from tmdb_mp4 import tmdb_mp4
-            from autoprocess import plex
-            from post_processor import PostProcessor
-        except ImportError:
-            log.error('Path to script folder appears to be invalid.')
-            return False
-
-        settings = ReadSettings(path, "autoProcess.ini")
-        converter = MkvtoMp4(settings)
+        exec_me = os.path.join(path, "postCouchPotato.py")
 
         try:
             imdbid = group['library']['identifier']
@@ -43,41 +31,15 @@ class PostProcess(Plugin):
         moviefile = group['renamed_files']
         original = group['files']['movie'][0]
 
-        success = False
+        success = True
 
         for inputfile in moviefile:
             try:
-                log.info('Processing file: %s', inputfile)
-                if MkvtoMp4(settings).validSource(inputfile):
-                    log.info('File is valid')
-                    output = converter.process(inputfile, original=original)
-
-                    # Tag with metadata
-                    if settings.tagfile:
-                        log.info('Tagging file with IMDB ID %s', imdbid)
-                        tagmp4 = tmdb_mp4(imdbid, original=original, language=settings.taglanguage)
-                        tagmp4.setHD(output['x'], output['y'])
-                        tagmp4.writeTags(output['output'], settings.artwork)
-
-                    #QTFS
-                    if settings.relocate_moov:
-                        converter.QTFS(output['output'])
-
-                    # Copy to additional locations
-                    output_files = converter.replicate(output['output'])
-
-                    # Run any post process scripts
-                    if settings.postprocess:
-                        post_processor = PostProcessor(output_files, log)
-                        post_processor.setMovie(imdbid)
-                        post_processor.run_scripts()
-
-                    success = True
-                else:
-                    log.info('File is invalid')
+                subprocess.Popen([exec_me, imdbid, inputfile, original], shell = True)
+                log.info("Executing post processing on file %s" % inputfile)
             except:
-                log.error('File processing failed: %s', (traceback.format_exc()))
-
-        plex.refreshPlex(settings, 'movie', log)
+                log.error("Failed to execute post processing on file %s" % inputfile)
+                log.error(traceback.format_exc())
+                success = False
 
         return success
