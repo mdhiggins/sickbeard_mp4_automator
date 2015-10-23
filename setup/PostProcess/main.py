df458eccf780b408c40e78e4a791c3b7ba1670ab
michael <mdhiggins23@gmail.com>
2015-10-23 15:02:23 -0400

    osx/linux fix
    only windows needs Shell=True

--- setup/PostProcess/main.py
+++ setup/PostProcess/main.py
@@ -35,7 +35,7 @@ class PostProcess(Plugin):
 
         for inputfile in moviefile:
             try:
-                subprocess.Popen([exec_me, imdbid, inputfile, original], shell = True)
+                subprocess.Popen([exec_me, imdbid, inputfile, original], shell = (os.name == 'nt'))
                 log.info("Executing post processing on file %s" % inputfile)
             except:
                 log.error("Failed to execute post processing on file %s" % inputfile)
