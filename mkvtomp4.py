f6a1bda6cc45c4fc29eb8e15ed4c96cd74489050
michael <mdhiggins23@gmail.com>
2016-01-09 11:14:00 -0500

    video-bitrate
    Adjustments to allow video-bitrate to override initial bitrate when not
    copying/remuxing video streams

--- mkvtomp4.py
+++ mkvtomp4.py
@@ -258,8 +258,12 @@ class MkvtoMp4:
         except:
             vbr = info.format.bitrate / 1000
 
-        vcodec = 'copy' if info.video.codec.lower() in self.video_codec else self.video_codec[0]
-        vbitrate = vbr
+        if info.video.codec.lower() in self.video_codec:
+            vcodec = 'copy'
+            vbitrate = None
+        else:
+            vcodec = self.video_codec[0]
+            vbitrate = self.video_bitrate if self.video_bitrate else vbr
 
         self.log.info("Pix Fmt: %s." % info.video.pix_fmt)
         if self.pix_fmt and self.pix_fmt.lower() != info.video.pix_fmt.lower():
