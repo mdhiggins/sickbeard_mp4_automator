bdd29a12adf12072db127a384612f9b44302f81d
Michael Higgins <mdhiggins23@gmail.com>
2018-11-02 20:49:07 -0400

    Revert "importSettings deepcopy"
    This reverts commit 8a082bba76a0f97b426739a3baebb136f395ef9f.

--- mkvtomp4.py
+++ mkvtomp4.py
@@ -5,7 +5,6 @@ import json
 import sys
 import shutil
 import logging
-from copy import deepcopy
 from converter import Converter, FFMpegConvertError
 from extensions import valid_input_extensions, valid_output_extensions, bad_subtitle_codecs, valid_subtitle_extensions, subtitle_codec_extensions, valid_tagging_extensions
 from babelfish import Language
@@ -123,7 +122,6 @@ class MkvtoMp4:
         self.deletesubs = set()
 
     def importSettings(self, settings):
-        settings = deepcopy(settings)
         self.FFMPEG_PATH = settings.ffmpeg
         self.FFPROBE_PATH = settings.ffprobe
         self.threads = settings.threads
