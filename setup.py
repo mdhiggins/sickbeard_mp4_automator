e0d33f9c216ff61679c36d4bb6ced168962154cf
Ross Patterson <me@rpatterson.net>
2019-08-01 22:35:37 -0700

    Python: Update project distribution metadata

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="sickbeard_mp4_automator",
+    version="1.0",
+    author="Michael Higgins",
+    author_email="mdhiggins23@gmail.com",
+    description=(
+        "Automatically convert video files to a standardized mp4 format with "
+        "proper metadata tagging to create a beautiful and uniform media "
+        "library."),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mdhiggins/sickbeard_mp4_automator",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "Natural Language :: English",
+        "Topic :: Multimedia :: Video :: Conversion",
     ],
 )
