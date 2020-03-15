8454b6b4bd05b64a96ca5fa7b9fcc7adb0fb3e02
Michael Higgins <mdhiggins23@gmail.com>
2020-03-15 10:20:10 -0400

    logging update
    logging.ini not included in favor of sample file
    logging.ini defaults handled similiar to autoProcess.ini and file created if missing
    removed SMA_LOGLOCAL env variable now that logging.ini is in config folder, you can simply edit the path now in logging.ini

--- resources/log.py
+++ resources/log.py
@@ -2,6 +2,93 @@ import logging
 import os
 import sys
 from logging.config import fileConfig
+try:
+    from configparser import RawConfigParser
+except ImportError:
+    from ConfigParser import RawConfigParser
+
+defaults = {
+    'loggers': {
+        'keys': 'root, manual, nzbget',
+    },
+    'handlers': {
+        'keys': 'consoleHandler, nzbgetHandler, fileHandler, manualHandler',
+    },
+    'formatters': {
+        'keys': 'simpleFormatter, minimalFormatter, nzbgetFormatter',
+    },
+    'logger_root': {
+        'level': 'DEBUG',
+        'handlers': 'consoleHandler, fileHandler',
+    },
+    'logger_nzbget': {
+        'level': 'DEBUG',
+        'handlers': 'nzbgetHandler, fileHandler',
+        'propagate': 0,
+        'qualname': 'NZBGetPostProcess',
+    },
+    'logger_manual': {
+        'level': 'DEBUG',
+        'handlers': 'manualHandler, fileHandler',
+        'propagate': 0,
+        'qualname': 'MANUAL',
+    },
+    'handler_consoleHandler': {
+        'class': 'StreamHandler',
+        'level': 'INFO',
+        'formatter': 'simpleFormatter',
+        'args': '(sys.stdout,)',
+    },
+    'handler_nzbgetHandler': {
+        'class': 'StreamHandler',
+        'level': 'INFO',
+        'formatter': 'nzbgetFormatter',
+        'args': '(sys.stdout,)',
+    },
+    'handler_manualHandler': {
+        'class': 'StreamHandler',
+        'level': 'INFO',
+        'formatter': 'minimalFormatter',
+        'args': '(sys.stdout,)',
+    },
+    'handler_fileHandler': {
+        'class': 'handlers.RotatingFileHandler',
+        'level': 'INFO',
+        'formatter': 'simpleFormatter',
+        'args': "('%(logfilename)s', 10000, 1)",
+    },
+    'formatter_simpleFormatter': {
+        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        'datefmt': '%Y-%m-%d %H:%M:%S',
+    },
+    'formatter_minimalFormatter': {
+        'format': '%(message)s',
+        'datefmt': ''
+    },
+    'formatter_nzbgetFormatter': {
+        'format': '[%(levelname)s] %(message)s',
+        'datefmt': ''
+    }
+}
+
+
+def checkLoggingConfig(configfile):
+    write = True
+    config = RawConfigParser()
+    if os.path.exists(configfile):
+        config.read(configfile)
+        write = False
+    for s in defaults:
+        if not config.has_section(s):
+            config.add_section(s)
+            write = True
+        for k in defaults[s]:
+            if not config.has_option(s, k):
+                config.set(s, k, str(defaults[s][k]))
+    if write:
+        fp = open(configfile, "w")
+        config.write(fp)
+        fp.close()
 
 
 def getLogger(name=None, custompath=None):
@@ -17,8 +104,7 @@ def getLogger(name=None, custompath=None):
         rootpath = os.path.abspath(os.path.join(resourcepath, "../"))
         configpath = os.path.join(rootpath, "config")
 
-    loglocal = os.environ.get("SMA_LOGLOCAL", "false").lower() in ['true', 'yes', 't', '1', 'y']
-    if os.name == 'nt' or loglocal:
+    if os.name == 'nt':
         logpath = configpath
     else:
         logpath = '/var/log'
@@ -30,6 +116,8 @@ def getLogger(name=None, custompath=None):
             logpath = configpath
 
     configfile = os.path.abspath(os.path.join(configpath, 'logging.ini')).replace("\\", "\\\\")
+    checkLoggingConfig(configfile)
+
     logfile = os.path.abspath(os.path.join(logpath, 'sma.log')).replace("\\", "\\\\")
     fileConfig(configfile, defaults={'logfilename': logfile})
 
