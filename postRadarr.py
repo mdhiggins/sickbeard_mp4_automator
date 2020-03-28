ae0498b592d6adb5668c283fa1c70e135dd3ee45
Michael Higgins <mdhiggins23@gmail.com>
2020-03-28 09:04:46 -0400

    double rescan

--- postRadarr.py
+++ postRadarr.py
@@ -8,6 +8,46 @@ from resources.readsettings import ReadSettings
 from resources.metadata import MediaType
 from resources.mediaprocessor import MediaProcessor
 
+
+def rescanAndWait(host, port, webroot, apikey, protocol, movieid, log, retries=6, delay=10):
+    headers = {'X-Api-Key': apikey}
+    # First trigger rescan
+    payload = {'name': 'RescanMovie', 'movieId': movieid}
+    url = protocol + host + ":" + str(port) + webroot + "/api/command"
+    r = requests.post(url, json=payload, headers=headers)
+    rstate = r.json()
+    try:
+        rstate = rstate[0]
+    except:
+        pass
+    log.info("Radarr response: ID %d %s." % (rstate['id'], rstate['state']))
+    log.debug(str(rstate))
+
+    # Then wait for it to finish
+    url = protocol + host + ":" + str(port) + webroot + "/api/command/" + str(rstate['id'])
+    log.info("Waiting rescan to complete")
+    r = requests.get(url, headers=headers)
+    command = r.json()
+    attempts = 0
+    while command['state'].lower() not in ['complete', 'completed'] and attempts < retries:
+        log.info(str(command['state']))
+        time.sleep(delay)
+        r = requests.get(url, headers=headers)
+        command = r.json()
+        attempts += 1
+
+    log.debug(str(command))
+    return command['state'].lower() in ['complete', 'completed']
+
+
+def getMovieInformation(host, port, webroot, apikey, protocol, movieid, log):
+    url = protocol + host + ":" + str(port) + webroot + "/api/movie/" + movieid
+    log.info("Requesting updated information from Radarr for movie ID %s." % movieid)
+    r = requests.get(url, headers=headers)
+    payload = r.json()
+    return payload
+
+
 log = getLogger("RadarrPostProcess")
 
 log.info("Radarr extra script post processing started.")
@@ -22,12 +62,14 @@ log.debug(os.environ)
 inputfile = os.environ.get('radarr_moviefile_path')
 original = os.environ.get('radarr_moviefile_scenename')
 imdbid = os.environ.get('radarr_movie_imdbid')
+movieid = os.environ.get('radarr_movie_id')
 
 mp = MediaProcessor(settings)
 
 log.debug("Input file: %s." % inputfile)
 log.debug("Original name: %s." % original)
 log.debug("IMDB ID: %s." % imdbid)
+log.debug("Radarr Movie ID: %s." % movieid)
 
 try:
     success = mp.fullprocess(inputfile, MediaType.Movie, original=original, imdbid=imdbid)
@@ -39,66 +81,42 @@ try:
             port = settings.Radarr['port']
             webroot = settings.Radarr['webroot']
             apikey = settings.Radarr['apikey']
-            if apikey != '':
-                try:
-                    ssl = int(settings.Radarr['ssl'])
-                except:
-                    ssl = 0
-                if ssl:
-                    protocol = "https://"
-                else:
-                    protocol = "http://"
+            ssl = settings.Radarr['ssl']
+            protocol = "https://" if ssl else "http://"
 
-                movieID = os.environ.get('radarr_movie_id')
-                log.debug("Radarr host: %s." % host)
-                log.debug("Radarr port: %s." % port)
-                log.debug("Radarr webroot: %s." % webroot)
-                log.debug("Radarr apikey: %s." % apikey)
-                log.debug("Radarr protocol: %s." % protocol)
-                log.debug("Radarr radarr_movie_id: %s." % movieID)
+            log.debug("Radarr host: %s." % host)
+            log.debug("Radarr port: %s." % port)
+            log.debug("Radarr webroot: %s." % webroot)
+            log.debug("Radarr apikey: %s." % apikey)
+            log.debug("Radarr protocol: %s." % protocol)
+
+            if apikey != '':
                 headers = {'X-Api-Key': apikey}
 
-                # First trigger rescan
-                payload = {'name': 'RescanMovie', 'movieId': movieID}
-                url = protocol + host + ":" + str(port) + webroot + "/api/command"
-                r = requests.post(url, json=payload, headers=headers)
-                rstate = r.json()
-                try:
-                    rstate = rstate[0]
-                except:
-                    pass
-                log.info("Radarr response: ID %d %s." % (rstate['id'], rstate['state']))
-                log.debug(str(rstate))
-
-                # Then wait for it to finish
-                url = protocol + host + ":" + str(port) + webroot + "/api/command/" + str(rstate['id'])
-                log.info("Waiting rescan to complete")
-                r = requests.get(url, headers=headers)
-                command = r.json()
-                attempts = 0
-                while command['state'].lower() not in ['complete', 'completed'] and attempts < 6:
-                    log.info(str(command['state']))
-                    time.sleep(10)
-                    r = requests.get(url, headers=headers)
-                    command = r.json()
-                    attempts += 1
-
-                log.debug(str(command))
-                if command['state'].lower() in ['complete', 'completed']:
+                if rescanAndWait(host, port, webroot, apikey, protocol, movieid, log):
                     log.info("Rescan command completed")
 
-                    # Then get movie information
-                    url = protocol + host + ":" + str(port) + webroot + "/api/movie/" + movieID
-                    log.info("Requesting updated information from Radarr for movie ID %s." % movieID)
-                    r = requests.get(url, headers=headers)
-                    payload = r.json()
+                    payload = getMovieInformation(host, port, webroot, apikey, protocol, movieid, log)
+                    if not payload.get('hasFile'):
+                        log.warning("Rescanned movie does not have a file, attempting second rescan.")
+                        if rescanAndWait(host, port, webroot, apikey, protocol, movieid, log):
+                            payload = getMovieInformation(host, port, webroot, apikey, protocol, movieid, log)
+                            if not payload.get('hasFile'):
+                                log.warning("Rescanned movie still does not have a file, will not set to monitored to prevent endless loop.")
+                                sys.exit(1)
+                            else:
+                                log.info("File found after second rescan.")
+                        else:
+                            log.error("Rescan command timed out")
+                            sys.exit(1)
+
                     payload['monitored'] = True
 
                     # Then set that movie to monitored
                     log.debug("Sending PUT request with following payload:")
                     log.debug(str(payload))  # debug
 
-                    url = protocol + host + ":" + str(port) + webroot + "/api/movie/" + str(movieID)
+                    url = protocol + host + ":" + str(port) + webroot + "/api/movie/" + str(movieid)
                     r = requests.put(url, json=payload, headers=headers)
                     success = r.json()
 
