0b65bfee24902140d244290f55fb3992470a21d4
Michael Higgins <mdhiggins23@gmail.com>
2020-06-11 05:49:39 -0400

    fix burn subtitle relative index calculation

--- resources/mediaprocessor.py
+++ resources/mediaprocessor.py
@@ -970,14 +970,14 @@ class MediaProcessor:
 
     def burnSubtitleFilter(self, inputfile, subtitle_streams, swl, valid_external_subs=None):
         if self.settings.burn_subtitles:
-            subtitle_streams = [x for x in subtitle_streams if self.validLanguage(x.metadata.get('language'), swl)]
-            subtitle_streams = sorted(subtitle_streams, key=lambda x: swl.index(x.metadata.get('language')) if x.metadata.get('language') in swl else 999)
+            filtered_subtitle_streams = [x for x in subtitle_streams if self.validLanguage(x.metadata.get('language'), swl)]
+            filtered_subtitle_streams = sorted(filtered_subtitle_streams, key=lambda x: swl.index(x.metadata.get('language')) if x.metadata.get('language') in swl else 999)
             sub_candidates = []
-            if len(subtitle_streams) > 0:
+            if len(filtered_subtitle_streams) > 0:
                 first_index = sorted([x.index for x in subtitle_streams])[0]
 
                 # Filter subtitles to be burned based on setting
-                sub_candidates = [x for x in subtitle_streams if self.checkDisposition(self.settings.burn_dispositions, x.disposition)]
+                sub_candidates = [x for x in filtered_subtitle_streams if self.checkDisposition(self.settings.burn_dispositions, x.disposition)]
                 # Filter out image based subtitles (until we can find a method to get this to work)
                 for x in sub_candidates[:]:
                     try:
