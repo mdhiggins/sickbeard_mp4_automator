a4b99a65a088627213cb520609d29c05631973d9
Michael Higgins <mdhiggins23@gmail.com>
2021-01-02 14:03:54 -0500

    Update extensions.py
    more pgs

--- resources/extensions.py
+++ resources/extensions.py
@@ -3,6 +3,7 @@ tmdb_api_key = "45e408d2851e968e6e4d0353ce621c66"
 subtitle_codec_extensions = {'srt': 'srt',
                              'webvtt': 'vtt',
                              'ass': 'ass',
-                             'pgs': 'sup'}
+                             'pgs': 'sup',
+                             'hdmv_pgs_subtitle': 'sup'}
 bad_post_files = ['resources', '.DS_Store']
 bad_post_extensions = ['.txt', '.log', '.pyc']
