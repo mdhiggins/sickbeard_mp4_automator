03ce803ea26f5b3c4c672189a9b3038664a417cc
rmangaha <rmangaha@gmail.com>
2021-05-19 09:32:04 -0700

    Update plex_autoscan.py
    Adjusted Plex Refresh

--- post_process/plex_autoscan.py
+++ post_process/plex_autoscan.py
@@ -6,7 +6,7 @@ import requests
 
 def main():
     print("plex_autoscan Post-processing Script")
-    url = "http://localhost:3689/apikey"
+    url = "http://plex:32400/library/sections/2/refresh?X-Plex-Token=SkTvJni8e9PmrA3TQywZ&path="
 
     files = json.loads(os.environ.get('SMA_FILES'))
     if not (files):
@@ -17,7 +17,8 @@ def main():
         dirname = os.path.dirname(filename)
         payload = {'eventType': 'Manual', 'filepath': dirname}
     try:
-        requests.post(url, json=payload)
+        print("Sending request to: " + url + dirname)
+        requests.get(url+dirname)
     except Exception as e:
         print("Error - Request failed")
         print(e)
