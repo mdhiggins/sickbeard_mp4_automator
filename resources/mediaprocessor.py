9d6bfe0c088929d187bfb96b53eed44b4819b832
Michael Higgins <mdhiggins23@icloud.com>
2022-09-11 18:54:12 -0400

    Update mediaprocessor.py

--- resources/mediaprocessor.py
+++ resources/mediaprocessor.py
@@ -1186,7 +1186,7 @@ class MediaProcessor:
                 elif not image_based and self.settings.embedsubs and self.settings.scodec and len(self.settings.scodec) > 0:
                     if (self.settings.cleanit and cleanit) or (self.settings.ffsubsync and ffsubsync):
                         try:
-                            scodec = 'copy' if s.codec in self.settings.scodec else self.settings.scodec[0]
+                            scodec = 'copy' if s.codec in ['srt', 'ass', 'webvtt'] else 'srt'
                             rips = self.ripSubs(inputfile, [self.generateRipSubOpts(inputfile, s, scodec)])
                             if rips:
                                 new_sub_path = rips[0]
