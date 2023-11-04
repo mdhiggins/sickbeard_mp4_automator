ced493e63fcc32f353e76d9d8fe14398eb703770
Michael Higgins <mdhiggins23@icloud.com>
2023-11-04 13:57:25 -0400

    Update jellyfin.py

--- setup/post_process/jellyfin.py
+++ setup/post_process/jellyfin.py
@@ -6,16 +6,16 @@ TOKEN = ""
 
 def main():
     print("Jellyfin Post-Processing Refresh Script")
-    url = "https://jellyfin.tld/library/refresh"
+    url = "https://jellyfin.tld:8096/Library/Refresh"
 
     headers = {
         "X-MediaBrowser-Token": TOKEN
     }
 
     try:
-        r = requests.get(url, headers=headers)
+        r = requests.post(url, headers=headers)
         print(r.status_code)
-        print(r.json())
+        print(r.text)
     except Exception as e:
         print("Error - Request failed")
         print(e)
