e62addfc7e68009e7dadde2aae2a6e0d9b5e40d4
Michael Higgins <mdhiggins23@icloud.com>
2024-02-29 06:35:06 -0500

    fix index out-of-bounds error

--- converter/ffmpeg.py
+++ converter/ffmpeg.py
@@ -542,13 +542,13 @@ class FFMpeg(object):
         prefix = "Supported pixel formats:"
         formatline = next((line.strip() for line in self._get_stdout([self.ffmpeg_path, '-hide_banner', '-h', 'encoder=%s' % encoder]).split('\n')[1:] if line and line.strip().startswith(prefix)), "")
         formats = formatline.split(":")
-        return formats[1].strip().split(" ") if formats and len(formats) > 0 else []
+        return formats[1].strip().split(" ") if formats and len(formats) > 1 else []
 
     def decoder_formats(self, decoder):
         prefix = "Supported pixel formats:"
         formatline = next((line.strip() for line in self._get_stdout([self.ffmpeg_path, '-hide_banner', '-h', 'decoder=%s' % decoder]).split('\n')[1:] if line and line.strip().startswith(prefix)), "")
         formats = formatline.split(":")
-        return formats[1].strip().split(" ") if formats and len(formats) > 0 else []
+        return formats[1].strip().split(" ") if formats and len(formats) > 1 else []
 
     @staticmethod
     def _spawn(cmds):
